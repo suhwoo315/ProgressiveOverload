1e586b864e4de2ff6de5ea45848138e76fec5a55
Veronica Suhwoo Yoon <yeopil315@gmail.com>
2023-05-29 14:29:20 +0900

    Trying bodytracking

--- sketch.js
+++ sketch.js
@@ -1,4 +1,15 @@
+let phase = 0;
 let stage1;
+
+let video;
+let poseNet;
+let poses = [];
+
+let num = 10;
+let x_values = [];
+let y_values = [];
+let x_mean = 0;
+let y_mean = 0;
 
 function preload(){
   stage1 = new Stage1();
@@ -6,9 +17,57 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // poseNet = ml5.poseNet(video, modelReady);
+  poseNet = ml5.poseNet(video);
+  poseNet.on("pose", function (results) {
+    poses = results;
+  });
+
+  video.hide();
 }
 
 function draw() {
   background(100);
-  stage1.display();
+  track();
+}
+
+// function modelReady() {
+//   select("#status").html("Model Loaded");
+// }
+
+function track(){
+  let result = [];
+
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      let keypoint = pose.keypoints[j];
+      if (j == 10) { // 
+        if (keypoint.score > 0.3) {
+          if (x_values.length < num) {
+            x_values.push(keypoint.position.x);
+            y_values.push(keypoint.position.y);
+          }
+
+          if (x_values.length == num) {
+            x_mean = 0;
+            for (let i = 0; i < num; i++) x_mean += x_values[i];
+            x_mean /= num;
+            x_values = [];
+
+            y_mean = 0;
+            for (let i = 0; i < num; i++) y_mean += y_values[i];
+            y_mean /= num;
+            y_values = [];
+          }
+        }
+      }
+    }
+  }
+
+  return result;
 }
--- stage1.js
+++ stage1.js
@@ -10,21 +10,21 @@ class Stage1 {
 
     display(){
         if (this.count < 10){
-            if ((frameCount % 10) == 0){
-                this.animIndex = ++this.animIndex % this.anim.length;
-            }
-            image(this.anim[this.animIndex], 0, 0, 700, 700);
+            // if ((frameCount % 10) == 0){
+            //     this.animIndex = ++this.animIndex % this.anim.length;
+            // }
+            imageMode(CENTER);
+            image(this.anim[this.animIndex], width/2, height/2, 400, 400);
         }
         else {
-
+            fill(255);
+            textSize(32);
+            text('You have done 10 dumbbell curls', 10, 30);
         }
     }
 
-    track(){
+    check(x, y){
         if (this.count < 10){
-
-        }
-        else {
 
         }
     }
